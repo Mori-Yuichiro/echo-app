1ca53f294c1f3755c0f51593cfa2a03ec93a2927
森祐一朗 <apotikisin4632@gmail.com>
2024-10-26 17:00:12 +0900

    データベース　初期設定

---
+++ api/model/user.go
@@ -0,0 +1,34 @@
+package model
+
+import "time"
+
+type User struct {
+	ID              uint      `json:"id" gorm:"primaryKey"`
+	Email           string    `json:"email" gorm:"unique"`
+	Password        string    `json:"password"`
+	Name            string    `json:"name"`
+	Image           string    `json:"image"`
+	DisplayName     string    `json:"display_name"`
+	PhoneNumber     string    `json:"phone_number"`
+	Bio             string    `json:"bio"`
+	Location        string    `json:"location"`
+	Website         string    `json:"website"`
+	Birthday        string    `json:"birthday"`
+	ProfileImageUrl string    `json:"profile_image_url"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+}
+
+type UserResponse struct {
+	ID              uint   `json:"id" gorm:"primaryKey"`
+	Email           string `json:"email" gorm:"unique"`
+	Name            string `json:"name"`
+	Image           string `json:"image"`
+	DisplayName     string `json:"display_name"`
+	PhoneNumber     string `json:"phone_number"`
+	Bio             string `json:"bio"`
+	Location        string `json:"location"`
+	Website         string `json:"website"`
+	Birthday        string `json:"birthday"`
+	ProfileImageUrl string `json:"profile_image_url"`
+}
