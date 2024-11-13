ebb45b343def89017eff056e39a4783f6cb7899e
森祐一朗 <apotikisin4632@gmail.com>
2024-11-13 16:29:14 +0900

    プロフィール　編集　hook

--- frontend/fe/src/hooks/modal/useModalHook.ts
+++ frontend/fe/src/hooks/modal/useModalHook.ts
@@ -1,13 +1,18 @@
 import { ProfileType } from "@/app/types/profile";
 import axiosInstance from "@/lib/axiosInstance";
 import { getCsrfToken } from "@/lib/csrf_lib";
-import { fileRead, fileUpload } from "@/lib/file";
+import { fileRead, fileUpload, uploadImage } from "@/lib/file";
 import { profilePatchSchema, ProfilePatchSchemaType } from "@/lib/validation/profile";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { toggleOpenModal, toggleReload } from "@/store/slice/slice";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
+
+type ProfileImagesType = {
+    profile_image_url?: string;
+    image?: string;
+}
 
 export const useModalHook = (profile: ProfileType) => {
     const { instance } = axiosInstance();
@@ -35,6 +40,42 @@ export const useModalHook = (profile: ProfileType) => {
 
     const { display_name, bio, location, website } = profile;
 
+    const checkFormData = (data: ProfilePatchSchemaType) => {
+        if (
+            data.display_name === display_name ||
+            data.bio === bio ||
+            data.location === location ||
+            data.website === website
+        ) {
+            return true
+        }
+        return false;
+    }
+
+    const updateProfileImage = async () => {
+        let newProfileImagaData: string;
+        let newAvatarData: string;
+        let profileImages: ProfileImagesType = {};
+
+        if (profile.profile_image_url !== profileImageUrl) {
+            newProfileImagaData = await (async () => {
+                const imageUrl = await uploadImage(instance, profileImageUrl);
+                return imageUrl.data.ImageUrl;
+            })();
+            profileImages.profile_image_url = newProfileImagaData;
+        }
+
+        if (profile.image !== avatarUrl) {
+            newAvatarData = await (async () => {
+                const imageUrl = await uploadImage(instance, avatarUrl);
+                return imageUrl.data.ImageUrl;
+            })();
+            profileImages.image = newAvatarData;
+        }
+
+        return profileImages;
+    }
+
     const {
         register,
         handleSubmit,
@@ -49,22 +90,53 @@ export const useModalHook = (profile: ProfileType) => {
         }
     });
 
-    const saveProfile = async (newProfile: ProfilePatchSchemaType) => {
+    const saveProfile = async (data: ProfilePatchSchemaType) => {
         try {
             if (instance.defaults.headers.common["X-CSRF-Token"] === undefined) {
                 const csrf = await getCsrfToken();
                 instance.defaults.headers.common["X-CSRF-Token"] = csrf;
             }
 
-            const { status } = await instance.put<ProfileType>(
-                "/users",
-                newProfile,
-                { withCredentials: true }
-            );
+            const profileImages: ProfileImagesType = await updateProfileImage();
 
-            if (status === 200) {
-                dispatch(toggleOpenModal(!openModal));
-                dispatch(toggleReload(!reload));
+            if ("profile_image_url" in profileImages || "image" in profileImages) {
+                if (checkFormData(data)) {
+                    const { status } = await instance.put<ProfileType>(
+                        "/users",
+                        {
+                            ...data,
+                            ...profileImages
+                        },
+                        { withCredentials: true }
+                    );
+
+                    if (status === 200) {
+                        dispatch(toggleOpenModal(!openModal));
+                        dispatch(toggleReload(!reload));
+                    }
+                } else {
+                    const { status } = await instance.put<ProfileType>(
+                        "/users",
+                        { ...profileImages },
+                        { withCredentials: true }
+                    );
+                    if (status === 200) {
+                        dispatch(toggleOpenModal(!openModal));
+                        dispatch(toggleReload(!reload));
+                    }
+                }
+            } else {
+                if (checkFormData(data)) {
+                    const { status } = await instance.put(
+                        "/users",
+                        data,
+                        { withCredentials: true }
+                    );
+                    if (status === 200) {
+                        dispatch(toggleOpenModal(!openModal));
+                        dispatch(toggleReload(!reload));
+                    }
+                }
             }
         } catch (err) {
             console.error(err);
