b46b7a5d952af6bbf30187d2c15cd5d68c44705c
森祐一朗 <apotikisin4632@gmail.com>
2024-12-14 17:30:44 +0900

    tweet component修正

--- frontend/fe/src/components/tweet/Tweet.tsx
+++ frontend/fe/src/components/tweet/Tweet.tsx
@@ -77,14 +77,26 @@ export default function Tweet({ tweet }: { tweet: TweetType }) {
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 1024 1024"><path fill="currentColor" d="M512 896q-66 0-134-16q-34 40-69.5 69.5t-60 43.5t-47.5 21.5t-30.5 8.5t-10.5 1q26-57 30-124.5T176 786Q94 723 47 635T0 448q0-91 40.5-174t109-143T313 35.5T512 0t199 35.5T874.5 131t109 143t40.5 174t-40.5 174t-109 143T711 860.5T512 896z" /></svg>
                     </div>
-                    {(tweet.retweets && tweet.retweets.filter(ret => ret.userId === currentUser?.id)) ? (
-                        <div
-                            className="cursor-pointer flex items-center gap-x-2"
-                            onClick={onClickDeleteRetweet}
-                        >
-                            <svg className="text-green-300" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 42 42"><path fill="currentColor" d="m24.5 30.5l7.96 7.371L40.5 30.5h-5V9.549c0-2.5-.561-3.049-3-3.049h-18l6.641 6H29.5v18h-5zm-8-18L8.52 5.16L.5 12.5h5v21.049c0 2.5.62 2.951 3 2.951h18.32l-6.32-6h-9v-18h5z" /></svg>
-                            <p>{tweet.retweets.length}</p>
-                        </div>
+                    {tweet.retweets ? (
+                        <>
+                            {(tweet.retweets.filter(ret => ret.user_id === currentUser?.id).length > 0) ? (
+                                <div
+                                    className="cursor-pointer flex items-center gap-x-2"
+                                    onClick={onClickDeleteRetweet}
+                                >
+                                    <svg className="text-green-300" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 42 42"><path fill="currentColor" d="m24.5 30.5l7.96 7.371L40.5 30.5h-5V9.549c0-2.5-.561-3.049-3-3.049h-18l6.641 6H29.5v18h-5zm-8-18L8.52 5.16L.5 12.5h5v21.049c0 2.5.62 2.951 3 2.951h18.32l-6.32-6h-9v-18h5z" /></svg>
+                                    <p>{tweet.retweets.length}</p>
+                                </div>
+                            ) : (
+                                <div
+                                    className="cursor-pointer flex items-center gap-x-2"
+                                    onClick={onClickCreateRetweet}
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 42 42"><path fill="currentColor" d="m24.5 30.5l7.96 7.371L40.5 30.5h-5V9.549c0-2.5-.561-3.049-3-3.049h-18l6.641 6H29.5v18h-5zm-8-18L8.52 5.16L.5 12.5h5v21.049c0 2.5.62 2.951 3 2.951h18.32l-6.32-6h-9v-18h5z" /></svg>
+                                    <p>{tweet.retweets.length}</p>
+                                </div>
+                            )}
+                        </>
                     ) : (
                         <div
                             className="cursor-pointer flex items-center gap-x-2"
@@ -94,16 +106,29 @@ export default function Tweet({ tweet }: { tweet: TweetType }) {
                             <p>0</p>
                         </div>
                     )}
-                    {(tweet.favorites && tweet.favorites.filter(fav => fav.userId === currentUser?.id)) ? (
-                        <div className="cursor-pointer flex items-center gap-x-2">
-                            <div
-                                className="text-red-400"
-                                onClick={onClickDeleteFavorite}
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 20"><path fill="currentColor" d="m10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z" /></svg>
-                            </div>
-                            <span>{tweet.favorites.length}</span>
-                        </div>
+                    {tweet.favorites ? (
+                        <>
+                            {(tweet.favorites.filter(fav => fav.user_id === currentUser?.id).length > 0) ? (
+                                <div className="cursor-pointer flex items-center gap-x-2">
+                                    <div
+                                        className="text-red-400"
+                                        onClick={onClickDeleteFavorite}
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 20"><path fill="currentColor" d="m10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z" /></svg>
+                                    </div>
+                                    <span>{tweet.favorites.length}</span>
+                                </div>
+                            ) : (
+                                <div className="cursor-pointer flex items-center gap-x-2">
+                                    <div
+                                        onClick={onClickCreateFavorite}
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4.528a6 6 0 0 0-8.243 8.715l6.829 6.828a2 2 0 0 0 2.828 0l6.829-6.828A6 6 0 0 0 12 4.528zm-1.172 1.644l.465.464a1 1 0 0 0 1.414 0l.465-.464a4 4 0 1 1 5.656 5.656L12 18.657l-6.828-6.829a4 4 0 0 1 5.656-5.656z" /></svg>
+                                    </div>
+                                    <span>{tweet.favorites.length}</span>
+                                </div>
+                            )}
+                        </>
                     ) : (
                         <div className="cursor-pointer flex items-center gap-x-2">
                             <div
@@ -114,7 +139,7 @@ export default function Tweet({ tweet }: { tweet: TweetType }) {
                             <span>0</span>
                         </div>
                     )}
-                    {(tweet.bookmarks && tweet.bookmarks.filter(book => book.userId === currentUser?.id)) ? (
+                    {(tweet.bookmarks && tweet.bookmarks.filter(book => book.user_id === currentUser?.id).length > 0) ? (
                         <div onClick={onClickDeleteBookmark}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 384 512"><path fill="currentColor" d="M0 512V48C0 21.49 21.49 0 48 0h288c26.51 0 48 21.49 48 48v464L192 400L0 512z" /></svg>
                         </div>
